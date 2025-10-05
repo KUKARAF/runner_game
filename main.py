bb4283bae824bad8df150e9cb1c15f652b101890
Rafał Kukawski <rafal.kuka94@gmail.com>
2025-10-05 18:12:11 +0200

    refactor: remove debug print and add todo comments for API endpoints and mission monitoring logic

--- main.py
+++ main.py
@@ -8,5 +8,6 @@ mission = Mission(
 )
 
 mission_text = mission.generate_mission()
-print(mission_text)
+# amazint! that worked! now we need to: 1. expose a endpoint that shows our mission status (we need a urls.py file for that) /mission/mission_name/status (just reply with json) 2. expose a endpoint that shows all audio tracks per mission (json with url.mp3, status (new: latest not played, played: latest played, stale: old completed mission) we then need to add a for loop that check every x minutes (configured in settings) how much distance and how much time passed and if we reached our target (if yes start success condition) AI!
+#print(mission_text)
 
